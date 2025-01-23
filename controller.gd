8f11bab2e3da658ede1f891c040841ab84764e39
Anthony Libardi <libardi42@gmail.com>
2025-01-23 14:30:31 -0500

    Connect in editor

--- controller.gd
+++ controller.gd
@@ -1,15 +1,6 @@
 extends Node
 
 
-@onready var click_point: Area2D = $ClickPoint
-
-
-func _ready():
-    click_point.interact_empty.connect(on_interact_empty)
-    click_point.interact_object.connect(on_interact)
-    click_point.auto_move.connect(on_auto_move)
-
-
 func on_interact_empty():
     print("clicked on nothing")
 
