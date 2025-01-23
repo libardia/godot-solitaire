7912e99aec74381c50e9fec30d17ce9ef18c269c
Anthony Libardi <libardi42@gmail.com>
2025-01-23 15:59:21 -0500

    remove prints

--- controller.gd
+++ controller.gd
@@ -2,15 +2,14 @@ extends Node
 
 
 func on_interact_empty():
-    print("clicked on nothing")
+    pass
 
 
 func on_interact(clicked: Node2D):
-    print("clicked on ", clicked)
     if clicked is Card:
         var card := clicked as Card
         card.set_face_up(not card.face_up)
 
 
 func on_auto_move(clicked: Node2D):
-    print("try to auto move ", clicked)
+    pass
