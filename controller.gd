67b071411b904209faac8df91c86dd976de56712
Anthony Libardi <libardi42@gmail.com>
2025-01-23 14:28:22 -0500

    Click point shouldn't be global, actually

--- controller.gd
+++ controller.gd
@@ -1,17 +1,22 @@
 extends Node
 
 
+@onready var click_point: Area2D = $ClickPoint
+
+
 func _ready():
-    ClickPoint.interact_empty.connect(on_interact_empty)
-    ClickPoint.interact_object.connect(on_interact)
-    ClickPoint.auto_move.connect(on_auto_move)
+    click_point.interact_empty.connect(on_interact_empty)
+    click_point.interact_object.connect(on_interact)
+    click_point.auto_move.connect(on_auto_move)
 
 
 func on_interact_empty():
     print("clicked on nothing")
 
+
 func on_interact(clicked: Node2D):
     print("clicked on ", clicked)
+
 
 func on_auto_move(clicked: Node2D):
     print("try to auto move ", clicked)
